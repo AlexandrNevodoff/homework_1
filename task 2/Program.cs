8c34fe1038a5d5533fed395256801bd6898548c0
Alexandr Nevodov <nevodoff@gmail.com>
2022-11-26 17:16:03 +0300

    ibitial committe

---
+++ task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear ();
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a==b) Console.WriteLine("Числа равны");
+
+if (a>b) Console.WriteLine("Первое число больше");
+
+if (a<b) Console.WriteLine("Второе число больше");
+
---
+++ task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿//
+Console.Clear ();
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второеое число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max=b;
+if (max <c) max =c;
+Console.Write("Максимальное число:" ); Console.Write(max);
